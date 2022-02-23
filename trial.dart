3ecea71ff91ecc611f6d6b3d87327ff310abfeff
prashantprojection <py2738@gmail.com>
2022-02-23 11:49:25 +0530

    Practice set

---
+++ trial.dart
@@ -0,0 +1,58 @@
+void main() {
+  
+// var a = 2;
+ 
+// for(initialization ; condition ; updation)
+// while(condition){}
+// int i=1;
+
+// while(i<=10){
+//   print(i*a);
+//   i++;
+// }
+
+// do{
+// 
+// }while(condition)
+// int age = 16;
+
+// do{
+// print("Valid for Voting");
+// age--;
+// }while(age>12);
+
+// List, Set, Map
+
+// var list = ['23']; //List
+// //          [0,1,2,3]
+// print(list);
+// print(list.length);
+// print(list.reversed);
+// print(list.hashCode);
+// print(list.runtimeType);
+// print(list.first);
+// print(list.isEmpty);
+// print(list.isNotEmpty);
+// print(list.last);
+// print(list.single);
+
+// Map(key:value)
+// Map map = {1:"prashant",2:"Dheeraj", 3: "name"};
+// print(map);
+// map["city"] = "Agra";
+// map["state"] = "U.P.";
+// print(map);
+
+// Set set = {1,2,3,4,6};
+
+// print(set.runtimeType);
+// print(set);
+
+// Set set1 = {3,4,5,6,7,8};
+
+// set.addAll(set1);
+// print(set);
+
+
+
+}
